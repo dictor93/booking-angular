f417ae9ca4bcdcedcefd73c3daaf3307246c0eb2
zetov.ruslan <zetov.ruslan@gmail.com>
2019-03-11 15:11:40 +0200

    Developed detail component, add images

--- src/app/booking/api-methods.service.ts
+++ src/app/booking/api-methods.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {API_BASE_URL} from '../../config';
 
 @Injectable()
 export class ApiMethodsService {
 
   constructor(private _http: HttpClient) { }
-  private hotelsUrl: string = 'http://localhost:8080/hotels/';
+  private hotelsUrl: string = `${API_BASE_URL}hotels/`;
   public getHotels() {
     return this._http.get(this.hotelsUrl);
   }
